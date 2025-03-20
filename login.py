be760a90ac6a99ac6af42c58eadce3dc1103ca72
Bdinesh14 <dineshyadavbairagani@gmail.com>
2025-03-20 16:40:57 +0530

    n and user added

---
+++ login.py
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main;
+{
+  int n=9898;
+char user=dinesh;
+{
+  if(n==9898 && user==dinesh)
+printf("the login successfully");
+else
+printf("the user name is in valid");
+return 0;
+}
